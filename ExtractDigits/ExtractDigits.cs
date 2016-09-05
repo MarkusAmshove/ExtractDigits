3f3ca744eea7d2085533bf25d189b5c8df3154fb
Markus Amshove <scm@amshove.org>
2016-09-05 18:26:36 +0200

    Initial

---
+++ ExtractDigits/ExtractDigits.cs
@@ -0,0 +1,39 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtractDigits
+{
+    public class ExtractDigits
+    {
+        const long TheNumber = long.MaxValue;
+
+        [Benchmark]
+        public long[] ExtractDigitsModulo()
+        {
+            long number = TheNumber;
+            long divisor = 10;
+            long remainer = number % divisor;
+            List<long> list = new List<long>();
+            while (number > divisor)
+            {
+                list.Add(remainer);
+                number /= divisor;
+                remainer = number % divisor;
+            }
+            list.Add(number);
+            list.Reverse();
+            return list.ToArray();
+        }
+
+        [Benchmark]
+        public long[] ExtractDigitsString()
+        {
+            long[] arr = Array.ConvertAll(TheNumber.ToString().ToArray(), x => long.Parse(x.ToString()));
+            return arr;
+        }
+    }
+}
---
+++ ExtractDigits/Program.cs
@@ -0,0 +1,14 @@
+﻿using BenchmarkDotNet.Running;
+using System;
+
+namespace ExtractDigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<ExtractDigits>();
+            Console.ReadLine();
+        }
+    }
+}
